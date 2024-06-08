484e9d35fc2bb6477fdffd8a33538ca3b4769c5e
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-06-08 16:00:19 +0300

    update conftest

--- src/tests/functional/conftest.py
+++ src/tests/functional/conftest.py
@@ -1,20 +1,26 @@
 import asyncio
+from typing import Generator
 
 import aiohttp
 import pytest_asyncio
 from httpx import AsyncClient
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
-from db.postgres import Base
+from crud.user import user_crud
+from db.postgres import Base, get_session
 from main import app
 from models.entity import User, Role, ROLES
 from tests.settings import test_settings
 
 
-@pytest_asyncio.fixture
-async def test_db_session():
-    engine = create_async_engine(test_settings.test_database_url, echo=True, future=True)
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
+@pytest_asyncio.fixture(scope='session')
+async def db_session():
+    engine = create_async_engine(test_settings.test_database_url, echo=True, future=True, poolclass=NullPool)
+    async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -26,24 +32,25 @@ async def test_db_session():
     await engine.dispose()
 
 
+
 @pytest_asyncio.fixture
-async def superuser_authenticated_client(async_client, admin_user):
-    login_data = {"login": admin_user.login, "password": "testpassword"}
-    response = await async_client.post("/auth/login", json=login_data)
+async def admin_authenticated_client(api_session, admin_user):
+    login_data = {'login': admin_user.login, 'password': 'test_password'}
+    response = await api_session.post(f'{test_settings.service_url}/auth/login', json=login_data, ssl=False)
     assert response.status_code == 200
     cookies = response.cookies
-    async_client.cookies = cookies
-    yield async_client
+    api_session.cookies = cookies
+    yield api_session
 
 
 @pytest_asyncio.fixture(scope='session')
-async def async_client(event_loop):
+async def async_client():
     async with AsyncClient(app=app, base_url=test_settings.service_url) as client:
         yield client
 
 
-@pytest_asyncio.fixture(scope='session')
-def event_loop():
+@pytest_asyncio.fixture(scope="session")
+def event_loop() -> Generator:
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
@@ -51,27 +58,27 @@ def event_loop():
 
 @pytest_asyncio.fixture(scope='session')
 async def api_session(event_loop):
-    session = aiohttp.ClientSession()
+    session = aiohttp.ClientSession(trust_env=True)
     yield session
     await session.close()
 
 @pytest_asyncio.fixture
-async def admin_user(test_db_session, roles):
+async def admin_user(db_session, roles):
     admin_role = roles['admin']
-    superuser = User(login="admin_username", password="testpassword", role=admin_role)
-    test_db_session.add(superuser)
-    await test_db_session.commit()
-    await test_db_session.refresh(superuser)
-    yield superuser
+    admin_user = User(login='admin_username3',
+                      password='test_password',
+                      first_name='test_first_name',
+                      last_name='test_last_name',
+                      role=admin_role.id,)
+    db_session.add(admin_user)
+    await db_session.commit()
+    await db_session.refresh(admin_user)
+    yield admin_user
 
 
 @pytest_asyncio.fixture
-async def roles(test_db_session):
+async def roles(db_session):
     roles = {role_name: Role(name=role_name) for role_name in ROLES}
-    # {"user": Role(**DEFAULT_ROLE_DATA),
-    #     "admin": Role(**SUPERUSER_ROLE_DATA),
-    #     "testrole": Role(name="testrole", access_level=1)
-    # }
-    test_db_session.add_all(roles.values())
-    await test_db_session.commit()
+    db_session.add_all(roles.values())
+    await db_session.commit()
     yield roles
