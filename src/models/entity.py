5bf6b643bdac4c125285f890c6f8bb883eeaef1f
Yurii Kolpakov <yurii.kolpakov@newstream.ai>
2024-06-08 11:20:01 +0300

    role to init

--- src/models/entity.py
+++ src/models/entity.py
@@ -48,11 +48,12 @@ class User(Base):
     created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.utcnow)  # TODO заменить datetime на актуальный аналог
     role: Mapped[Role] = mapped_column(ForeignKey('roles.id'), default=1)
 
-    def __init__(self, login: str, password: str, first_name: str, last_name: str) -> None:
+    def __init__(self, login: str, password: str, first_name: str, last_name: str, role: Role = None) -> None:
         self.login = login
         self.password = self.password = generate_password_hash(password)
         self.first_name = first_name
         self.last_name = last_name
+        self.role = role
 
     def check_password(self, password: str) -> bool:
         return check_password_hash(self.password, password)
