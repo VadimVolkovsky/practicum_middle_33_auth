fb75eb4eaec71e7799703b86a99b679814d7224f
VadimVolkovsky <vadimas29@gmail.com>
2024-06-10 18:27:14 +0300

    убран scope=session

--- tests/functional/pytest_fixtures/roles.py
+++ tests/functional/pytest_fixtures/roles.py
@@ -3,7 +3,7 @@ import pytest_asyncio
 from models.entity import Role, ROLES
 
 
-@pytest_asyncio.fixture(scope='session')
+@pytest_asyncio.fixture
 async def roles(db_session):
     roles = {role_name: Role(name=role_name) for role_name in ROLES}
     db_session.add_all(roles.values())
